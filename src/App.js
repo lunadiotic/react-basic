27066d9107a5ee5dcf8fe77c7e9543d04b9a6f26
lunadiotic <ahmadhakim404@gmail.com>
2021-03-30 22:18:48 +0700

    trying multi-components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
 import './App.css';
+import Todo from "./components/todo/Todo";
 
-// function App() {
-//   return <h1 title="This is from title">Hello, World!</h1>
-//   // return React.createElement('h1', {title: 'This is from title'}, 'Hello, world!');
-// }
-
-class App extends React.Component {
-  render() {
-    return <h1 title="This is from title">Hello, World!</h1>
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/todo-list/TodoList.js
@@ -0,0 +1,13 @@
+import './TodoList.css'
+
+function TodoList() {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Tools</li>
+            <li>Record</li>
+        </ul>
+    );
+}
+
+export default TodoList
