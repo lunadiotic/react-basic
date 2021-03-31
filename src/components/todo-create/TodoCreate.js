9449e428c0ad4d2709d0c5216936405a2b61481a
unknown <ahmadhakim404@gmail.com>
2021-03-31 20:56:54 +0700

    two-way data binding input form using state

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -1,19 +1,30 @@
+import { useState } from 'react'
 import './TodoCreate.css'
 
 const TodoCreate = props => {
+    // let todo = '';
+    const [todo, setTodo] = useState('');
+
     const handleSubmit = (event) => {
         event.preventDefault()
         const newTodo = {
             id: (Math.floor(Math.random() * 100) + 1),
-            title: 'Learn react'
+            title: todo
         }
-
         props.onCreateTodo(newTodo)
+        // todo = ''
+        setTodo('')        
+    }
+
+    const handleTodoInput = event => {
+        // todo = event.target.value;
+        setTodo(event.target.value)
+        console.log(todo)
     }
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
-            <input type="text" />
+            <input type="text" value={todo} onChange={handleTodoInput}/>
             <button type="submit">Add</button>
         </form>
     );
